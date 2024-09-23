2bf9404b3409ff416d725fc1e229b544615286bc
Rayan <magirayan84@gmail.com>
2024-09-23 22:00:27 +0600

    Commit 12: did pattern 10

--- Patterns.cpp
+++ Patterns.cpp
@@ -362,6 +362,48 @@ void pattern_9(int n){
     }
 }
 
+//Pattern 10:
+/*
+Pattern - 10: Half Diamond Star Pattern
+Input Format: N = 6
+Result:   
+     *
+     **
+     *** 
+     ****
+     *****
+     ******  
+     *****
+     ****
+     ***    
+     **
+     *
+     Its a symmetrical pattern and its symmetry breaks if row exceeds the N 
+    for the outer loop: we can say the formula is 2n - 1
+    for the inner loops: 
+    we can say int stars = i
+                if (i>n) stars = 2n - i;
+                for j = 0; j <=stars; j++
+                    cout << "*"
+
+*/
+void pattern_10(int n){
+    for (int i = 1; i <= 2*n - 1; i++)
+    {
+        int stars = i;
+        if(i>n){
+            stars = 2*n-i;
+        }
+        for (int j = 1; j <=stars; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    
+}
+
+
 int main(){
 //Creating an integer to store the input --> n
 
@@ -373,6 +415,6 @@ cin >> n;
 
 //Showing output:
 
-pattern_9(n);
+pattern_10(n);
 
 }
